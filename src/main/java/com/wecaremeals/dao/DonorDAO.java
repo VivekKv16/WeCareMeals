71f98d3e49dcc41777a10ff42e7a592515d06dae
vivek <vivekkvacharya@gmail.com>
2025-11-09 19:21:01 +0530

    Impl Donor Login

--- src/main/java/com/wecaremeals/dao/DonorDAO.java
+++ src/main/java/com/wecaremeals/dao/DonorDAO.java
@@ -6,4 +6,5 @@ import java.sql.SQLException;
 
 public interface DonorDAO {
     boolean registerDonor(Donor donor) throws SQLException, ClassNotFoundException;
+    Donor loginDonor(long phone,String password) throws SQLException, ClassNotFoundException;
 }
--- src/main/java/com/wecaremeals/dao/DonorDAOImpl.java
+++ src/main/java/com/wecaremeals/dao/DonorDAOImpl.java
@@ -5,6 +5,7 @@ import com.wecaremeals.util.DbConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DonorDAOImpl implements com.wecaremeals.dao.DonorDAO{
@@ -25,13 +26,30 @@ public class DonorDAOImpl implements com.wecaremeals.dao.DonorDAO{
 
         int update=ps.executeUpdate();
 
-        System.out.println(donor.getName());
-        if(update>0){
-            System.out.println("entered successfully");
 
+        if(update>0){
             return true;
         }
-
         return false;
+    }
+
+    @Override
+    public Donor loginDonor(long phone, String password) throws SQLException, ClassNotFoundException {
+        String sqry="select * from donor where phone=? and password=?";
+        con=DbConnection.getConnection();
+        Donor donor=null;
+        PreparedStatement ps=con.prepareStatement(sqry);
+        ps.setLong(1,phone);
+        ps.setString(2,password);
+        ResultSet rs=ps.executeQuery();
+        if(rs.next()){
+            donor =new Donor();
+            donor.setDonorId(rs.getInt("donorId"));
+            donor.setName(rs.getString("name"));
+            donor.setPhone(rs.getLong("phone"));
+            donor.setPassword(rs.getString("password"));
+            donor.setAddress(rs.getString("address"));
+        }
+        return donor;
     }
 }
